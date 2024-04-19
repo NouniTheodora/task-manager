06128a04d2fcc574192188097638eeed5d0b6c37
Theodora Nouni <nounitheodora@gmail.com>
2024-04-19 17:31:53 +0300

    Register a new user (register route)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -25,6 +26,25 @@ class AuthController extends Controller
         $user = User::where('email', $validated['email'])->first();
 
         return response()->json([
+            'access_token'  => $user->createToken('api_token')->plainTextToken,
+            'token_type'    => 'Bearer',
+        ]);
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users,email', // should be unique in the users table / column email
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user = User::create($validated);
+
+        return response()->json([
+            'data'          => $user,
             'access_token'  => $user->createToken('api_token')->plainTextToken,
             'token_type'    => 'Bearer',
         ]);
